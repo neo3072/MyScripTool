fbef954d4c71aed06d2db178afefdaa8b1ae50a1
wildpointer <mail.gerissen@gmail.com>
2016-04-02 21:21:47 +0530

    Initial

---
+++ MyScripTool/JSONQuote.py
@@ -0,0 +1,30 @@
+'''
+Created on 02-Apr-2016
+
+@author: wildpointer
+'''
+import urllib2
+import json
+import time
+
+class GoogleFinanceAPI:
+    def __init__(self):
+        self.prefix="http://finance.google.com/finance/info?client=ig&q="
+
+    def get(self,symbol,exchange):
+        url=self.prefix+"%s:%s"%(exchange,symbol)
+        u=urllib2.urlopen(url)
+        content=u.read()
+
+        print content
+        return content
+        #obj=json.loads(content[3:])
+        #return obj[0]
+
+if __name__=="__main__":
+    c=GoogleFinanceAPI()
+
+while 1:
+    quote=c.get("RELIANCE","NSE")
+    print quote
+    time.sleep(15)
